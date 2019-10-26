30015ff8c5463c01f97f4d618e5903c59d94df29
MattMan569 <matthewpolsom@hotmail.com>
2019-10-25 21:12:18 -0600

    added tasks patch endpoint

--- src/index.js
+++ src/index.js
@@ -58,16 +58,15 @@ app.patch('/users/:id', async (req, res) => {
     const _id = req.params.id;
     const body = req.body;
     const allowedUpdates = ['name', 'email', 'password', 'age'];
-
     const updates = Object.keys(body);
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
-    if (!isValidOperation) {
-        return res.status(400).send({error: 'Invalid update field'});
-    }
-
     if (!mongoose.Types.ObjectId.isValid(_id)) {
         return res.status(400).send({error: 'Invalid id'});
+    }
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid update field'});
     }
 
     try {
@@ -129,6 +128,38 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task);
     } catch (e) {
         res.status(500).send(e);
+    }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    const body = req.body;
+    const allowedUpdates = ['description', 'completed'];
+    const updates = Object.keys(body);
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(400).send({error: 'Invalid id'});
+    }
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid update field'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send(task);
+    } catch (e) {
+        if (e._message == 'Validation failed') {
+            res.status(400).send(e);
+        } else {
+            res.status(500).send(e);
+        }
     }
 });
 
