17d7a317bc9d6fbf13530406a7d0c8cf6310ddc1
MattMan569 <matthewpolsom@hotmail.com>
2019-10-29 15:29:44 -0600

    removed get all users, replace with get current profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
+
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -27,18 +29,8 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-
-        if (!users) {
-            return res.status(404).send();
-        }
-
-        res.send(users);
-    } catch (e) {
-        res.status(500).send();
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
