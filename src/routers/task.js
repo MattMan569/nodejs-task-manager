3f49af1f40bb357fe5bba6e9a29d58558e2cfaff
MattMan569 <matthewpolsom@hotmail.com>
2019-10-30 15:43:18 -0600

    added match, pagination, and sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,9 +18,32 @@ router.post('/tasks', auth, async (req, res) => {
     }
 });
 
+// GET /tasks?completed=bool                - match
+// GET /tasks?limit=number&skip=number      - pagination
+// GET /tasks?sortBy=field:(asc|desc)       - sorting
 router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+    const sort = {};
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true';
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     try {
-        await req.user.populate('tasks').execPopulate();
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort,
+            },
+        }).execPopulate();
         res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send();
