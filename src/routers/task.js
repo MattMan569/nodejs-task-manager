28124b72c61b0366c8f1161c158c51a69c43cf07
MattMan569 <matthewpolsom@hotmail.com>
2019-10-30 11:54:22 -0600

    add auth to task routes

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Task = require('../models/task');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id,
+    });
 
     try {
         await task.save();
@@ -14,21 +18,16 @@ router.post('/tasks', async (req, res) => {
     }
 });
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({});
-
-        if (!tasks) {
-            return res.status(404).send();
-        }
-
-        res.send(tasks);
+        await req.user.populate('tasks').execPopulate();
+        res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send();
     }
 });
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
     if (!mongoose.Types.ObjectId.isValid(_id)) {
@@ -36,7 +35,8 @@ router.get('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(_id);
+        // Only return tasks the current user has created
+        const task = await Task.findOne({_id, owner: req.user._id});
 
         if (!task) {
             return res.status(404).send();
@@ -48,7 +48,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 });
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     const body = req.body;
     const allowedUpdates = ['description', 'completed'];
@@ -64,15 +64,14 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(_id);
-        updates.forEach((update) => task[update] = body[update]);
-        await task.save();
-
-        // const task = await Task.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
+        const task = await Task.findOne({_id, owner: req.user._id});
 
         if (!task) {
             return res.status(404).send();
         }
+
+        updates.forEach((update) => task[update] = body[update]);
+        await task.save();
 
         res.send(task);
     } catch (e) {
@@ -84,7 +83,7 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 });
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
     if (!mongoose.Types.ObjectId.isValid(_id)) {
@@ -92,7 +91,8 @@ router.delete('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndDelete(_id);
+        // const task = await Task.findByIdAndDelete(_id);
+        const task = await Task.findOneAndDelete({_id, owner: req.user._id});
 
         if (!task) {
             return res.status(404).send();
