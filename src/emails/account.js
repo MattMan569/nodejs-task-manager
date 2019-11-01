82ef4dc5e88234970c2e615ce07036ec2b133ed7
MattMan569 <matthewpolsom@hotmail.com>
2019-10-31 19:13:45 -0600

    setup welcome and cancelation emails

---
+++ src/emails/account.js
@@ -0,0 +1,28 @@
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.UdemyNodeJsTaskAppApiKey);
+
+const fromEmail = 'matthewpolsom@hotmail.com';
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: fromEmail,
+        subject: 'Welcome to Task App!',
+        text: `Welcome to the app, ${name}. Let me know how you find the app.`,
+    });
+};
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: fromEmail,
+        subject: 'Sorry to see you go',
+        text: `We're sorry to see that you're leaving Task App, ${name}. If you wouldn't mind, please let us know why you're leaving.`,
+    });
+};
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail,
+};
