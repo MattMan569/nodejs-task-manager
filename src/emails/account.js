b265303011cc9a6eea568cb5173c375733c42cc0
MattMan569 <matthewpolsom@hotmail.com>
2019-10-31 19:39:52 -0600

    changed environment variables

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.UdemyNodeJsTaskAppApiKey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const fromEmail = 'matthewpolsom@hotmail.com';
 
