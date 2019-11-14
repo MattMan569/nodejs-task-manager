99a8a52449bd0b7cf04843f502f89329acbd4b3f
MattMan569 <matthewpolsom@hotmail.com>
2019-11-13 19:33:31 -0600

    added additional task tests

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,21 +1,60 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const User = require('../../src/models/user');
+const Task = require('../../src/models/task');
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
     _id: userOneId,
     name: 'User One',
-    email: 'userone@email.com',
-    password: 'useronepass',
+    email: 'userOne@email.com',
+    password: 'user1pass',
     tokens: [{
         token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET),
     }],
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+    _id: userTwoId,
+    name: 'User Two',
+    email: 'userTwo@email.com',
+    password: 'user2pass',
+    tokens: [{
+        token: jwt.sign({_id: userTwoId}, process.env.JWT_SECRET),
+    }],
+};
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Task one',
+    completed: false,
+    owner: userOne._id,
+};
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Task two',
+    completed: true,
+    owner: userOne._id,
+};
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Task three',
+    owner: userTwo._id,
+};
+
 const setupDatabase = async () => {
     await User.deleteMany();
+    await Task.deleteMany();
+
     await new User(userOne).save();
+    await new User(userTwo).save();
+
+    await new Task(taskOne).save();
+    await new Task(taskTwo).save();
+    await new Task(taskThree).save();
 };
 
 const disconnectDatabase = async () => await mongoose.disconnect();
@@ -23,6 +62,8 @@ const disconnectDatabase = async () => await mongoose.disconnect();
 module.exports = {
     userOneId,
     userOne,
+    userTwo,
+    taskOne,
     setupDatabase,
     disconnectDatabase,
 };
--- tests/task.test.js
+++ tests/task.test.js
@@ -1,12 +1,10 @@
 const request = require('supertest');
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const {userOneId, userOne, setupDatabase, disconnectDatabase} = require('./fixtures/db');
+const {userOne, userTwo, taskOne, setupDatabase, disconnectDatabase} = require('./fixtures/db');
 
 beforeEach(setupDatabase);
-
-// Prevent Jest's open handles error
-afterAll(disconnectDatabase);
+afterAll(disconnectDatabase); // Prevent Jest's open handles error
 
 test('Create task for user', async () => {
     const response = await request(app)
@@ -20,4 +18,25 @@ test('Create task for user', async () => {
     const task = await Task.findById(response.body._id);
     expect(task).not.toBeNull();
     expect(task.completed).toBe(false);
+});
+
+test('Get tasks for user one', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+
+    expect(response.body.length).toBe(2);
+});
+
+test('Fail to delete user one\'s task as user two', async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404);
+
+    const task = Task.findById(taskOne._id);
+    expect(task).not.toBeNull();
 });
